fa816faa814ca860789fad3e03be1aa638f60c44
wise-keyboard-533b <lupitarivera8899@gmail.com>
2020-12-28 20:01:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,9 @@
 function basicTeenager(age)
-
+if (age < 13) {"you are a teen ager!"
+} else if (age > 19)
+   {"you are not a teen ager"
+} else (age > 19) {return"You are a grownup"}
+ (age < 13){return"you are a kid"
 {
 }
   }
